dd308f3b2414b2ff4dadf6524bfb5fba04ba3258
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-07 18:25:25 +0100

    Seccion 5 - 38 - Preparando el proyecto y creando el primer componente

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import {bootstrap} from "angular2/platform/browser";
+import {AppComponent} from "./app.components";
+import { ROUTER_PROVIDERS } from 'angular2/router';
+
+bootstrap(AppComponent, [ROUTER_PROVIDERS]);
