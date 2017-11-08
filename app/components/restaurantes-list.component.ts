cf1cdcc8122d7f6166a6297f36b32dccc38b0bb3
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-08 16:37:43 +0100

    Seccion 5 - 43 - Listado de restaurantes

--- app/components/restaurantes-list.component.ts
+++ app/components/restaurantes-list.component.ts
@@ -1,6 +1,7 @@
 import  {Component, OnInit} from "angular2/core";
 import { ROUTER_DIRECTIVES,RouteConfig,Router } from 'angular2/router';
 import { RestauranteService } from '../services/restaurante.service';
+import { Restaurante } from '../model/restaurante';
 
 @Component({
   selector: "restaurantes-list",
@@ -10,8 +11,33 @@ import { RestauranteService } from '../services/restaurante.service';
 
 export class RestaurantesListComponent implements OnInit{
   public titulo:string = "Listado de restaurantes:";
+  public restaurantes:Array<Restaurante>;
+  public status:string;
+  public errorMessage;
 
   constructor(private _restauranteService: RestauranteService){}
 
-  ngOnInit(){console.log('restaurantes-list component cargado');}
+  ngOnInit(){
+    this.getRestaurantes();
+    console.log('restaurantes-list component cargado');
+  }
+
+  getRestaurantes(){
+    this._restauranteService.getRestaurantes().subscribe(
+      result => {
+        this.restaurantes = result.data;
+        this.status = result.status;
+        if(this.status != 'success'){
+          alert('Error en el servidor');
+        }
+      },
+      error => {
+        this.errorMessage = <any>error;
+        if(this.errorMessage !== null){
+          console.log(this.errorMessage);
+          alert('Error en la petición');
+        }
+      }
+    );
+  };
 }
