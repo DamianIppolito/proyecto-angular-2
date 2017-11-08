840ab0c3da2fe7106415525d0da7c2d8bba9f01c
Damian Ippolito <dippolito@dimaticasoftware.com>
2017-11-08 12:42:47 +0100

    Seccion 5 - 41 - Crear una clase Restaurante(2)

---
+++ app/model/restaurante.ts
@@ -0,0 +1,10 @@
+export class Restaurante{
+  construnctor(
+    public id:number,
+    public nombre:string,
+    public direccion:string,
+    public descripcion:string,
+    public imagen:string,
+    public precio:string
+  ){}
+}
